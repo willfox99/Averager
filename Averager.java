1b27cfa2ffb6e57c6b7d7bf1d66878c0ba6cc7cc
Kent Collins <kent.r.collins@gmail.com>
2014-10-12 16:12:56 +0100

    Update Averager.java

--- Averager.java
+++ Averager.java
@@ -1,1 +1,16 @@
 /**
+*
+* Reads in scores until a negative value is encountered, at which point it determines and prints the average
+*
+* @author <your name here>
+* @version <date you finished the code>
+*
+*/
+
+public class Averager {
+	
+	public static void main(String[] args) {
+		// TODO - Put your code here
+	}
+
+}
