c86232dbe6a019a9d29685755bd253804e1d6592
willfox99 <william_fox@asl.org>
2014-10-14 09:28:52 +0100

    My Averager Code

--- Averager.java
+++ Averager.java
@@ -1,16 +1,29 @@
-/**
-*
-* Reads in scores until a negative value is encountered, at which point it determines and prints the average
-*
-* @author <your name here>
-* @version <date you finished the code>
-*
-*/
+//Will Fox
 
-public class Averager {
-	
-	public static void main(String[] args) {
-		// TODO - Put your code here
-	}
+import java.util.Scanner;
 
+public class Grades {
+    
+    public static void main(String []args) {
+        
+        Scanner sc = new Scanner(System.in);
+        double numInput = 0.0;
+        double totalNum = 0.0;
+        System.out.println("Enter the number you want to average (or -1 to exit): ");
+        double addNum = sc.nextDouble();
+        while(addNum != -1) {
+            
+            totalNum = totalNum + addNum;
+            numInput = numInput + 1;
+            System.out.println("Enter another number (or -1 to exit): ");
+            
+            addNum = sc.nextDouble();
+            
+            
+        }
+        
+        System.out.println("Your grade average is: " + totalNum / numInput);
+        
+    }
+    
 }
